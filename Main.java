e6687bc63ac330e975735041fe6368b3dfa31b35
yooInTheBuilding <dbals9926@gmail.com>
2023-12-04 21:06:31 +0900

    baek10810

--- Main.java
+++ Main.java
@@ -1,35 +1,38 @@
 package practice_alone;
-
 import java.io.*;
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
-		int[] arr1 = new int[9];
-		for (int i = 0; i < 9; i++){
-			arr1[i] = Integer.parseInt(br.readLine());
-		}
-		int max = arr1[0];
-		for (int i = 0; i < 9; i++){
-			if(max < arr1[i]){
-				max = arr1[i];
+		String s1 = br.readLine();
+		st = new StringTokenizer(s1);
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr1 = new int[N];
+		int I;
+		int J;
+		int K;
+		String s2;
+		for (int i = 1; i <= M; i++){
+			s2 = br.readLine();
+			st = new StringTokenizer(s2);
+			I = Integer.parseInt(st.nextToken());
+			J = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			for (int j = I-1; j < J; j++){
+				arr1[j] = K;
 			}
 		}
-		int check = 0;
-		for (int i = 0; i < 9; i++){
-			if(max == arr1[i]){
-				check = i + 1;
-			}
+		for (int i = 0; i < N; i++){
+			System.out.print(arr1[i] + " ");
 		}
-		bw.write(String.valueOf(max) + "\n");
-		bw.write(String.valueOf(check));
 
-		bw.close();
-		
+
 
 	}
 }
