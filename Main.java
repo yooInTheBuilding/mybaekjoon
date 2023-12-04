9cf3c082dc35184a71446f1f3052e8164917f94a
yooInTheBuilding <dbals9926@gmail.com>
2023-12-04 16:19:15 +0900

    baek10807

--- Main.java
+++ Main.java
@@ -1,30 +1,31 @@
 package practice_alone;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		int a;
-		int b;
-		int sum;
-		String n;
-		while((n = br.readLine()) != null){
-			st = new StringTokenizer(n);
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			sum = a + b;
-			bw.write(sum + "\n");
+
+		int N = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		st = new StringTokenizer(s);
+		int[] arr = new int[N];
+		int sum = 0;
+		int check = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for(int e = 0; e < N; e++){
+			if(arr[e] == check) {
+				sum++;
 			}
-		bw.close();
+		}
+		System.out.println(sum);
 		}
 
 	}
