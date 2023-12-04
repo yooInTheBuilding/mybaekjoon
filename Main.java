19a203672a5d67fab0ecf9da63f770dab62b67e5
yooInTheBuilding <dbals9926@gmail.com>
2023-12-04 11:05:47 +0900

    baek2438

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
 package practice_alone;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		String s = "";
+		
+		for(int i = 1; i <= N; i++) {
+			s += "*";
+			bw.write(s + "\n");
+		}
+		bw.close();
+		
 	}
 
 }
