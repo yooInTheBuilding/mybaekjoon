eb5a1ebd99b259fa32c4cf6b7ecdf5a12b7857aa
yooInTheBuilding <dbals9926@gmail.com>
2023-12-04 14:57:06 +0900

    Changes

--- Main.java
+++ Main.java
@@ -13,22 +13,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		int a = 0;
-		int b = 0;
-		int i = 0;
-		int sum = 0;
-		while(i < 1){
-			st = new StringTokenizer(br.readLine());
+		int a;
+		int b;
+		int sum;
+		String n;
+		while((n = br.readLine()) != null){
+			st = new StringTokenizer(n);
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
-			if(a == 0 && b == 0){
-				i++;
-			}else {
-				sum = a + b;
-				bw.write(sum + "\n");
+			sum = a + b;
+			bw.write(sum + "\n");
 			}
-		}
 		bw.close();
-	}
+		}
 
-}
+	}
