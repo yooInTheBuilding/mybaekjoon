cb11027677154e9369de325686af04736c4aa4ad
yooInTheBuilding <dbals9926@gmail.com>
2023-12-05 15:06:57 +0900

    baek10811

--- Main.java
+++ Main.java
@@ -1,28 +1,42 @@
 package practice_alone;
 import java.io.*;
-
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		int[] arr1 = new int[10];
-		boolean[] arr2 = new boolean[42];
-		int var1;
-		int sum = 0;
-		for (int i = 0; i < 10; i++){
-			var1 = Integer.parseInt(br.readLine());
-			arr1[i] = var1 % 42;
-			arr2[arr1[i]] = true;
+		String s1 = br.readLine();
+		st = new StringTokenizer(s1);
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int I;
+		int J;
+		String s2;
+		int[] arr1 = new int[N];
+		int[] arr2 = new int[N];
+		for (int i = 0; i < N; i++){
+			arr1[i] = i+1;
+			arr2[i] = i+1;
 		}
-		for (int i = 0; i < 42; i++){
-			if(arr2[i]){
-				sum++;
+		for (int i = 0; i < M; i++){
+			s2 = br.readLine();
+			st = new StringTokenizer(s2);
+			I = Integer.parseInt(st.nextToken());
+			J = Integer.parseInt(st.nextToken());
+			for (int j = I-1; j <= J-1; j++){
+				arr1[j] = arr2[I+J-2-j];
+			}
+			for (int j = 0; j < N; j++){
+				arr2[j] = arr1[j];
 			}
 		}
-		System.out.println(sum);
+		for (int i = 0; i < N; i++){
+			System.out.print(arr1[i] + " ");
+		}
 
 	}
 }
