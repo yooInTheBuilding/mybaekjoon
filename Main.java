d3189a79656022cdb6442e2ea7dc8a8278db07fb
yooInTheBuilding <dbals9926@gmail.com>
2023-12-05 12:12:47 +0900

    baek5597

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package practice_alone;
 import java.io.*;
-import java.util.StringTokenizer;
+
 
 
 public class Main {
@@ -8,33 +8,34 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s1 = br.readLine();
-		StringTokenizer st;
-		st = new StringTokenizer(s1);
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] arr1 = new int[N];
-		int I;
-		int J;
-		int originI;
-		int originJ;
-		String s2;
-		for (int i = 0; i < N; i++){
-			arr1[i] = i + 1;
+		int[] arr1 = new int[28];
+		int[] arr2 = new int[30];
+		boolean[] arr3 = new boolean[30];
+		int n;
+		for (int i = 0; i < 28; i++){
+			n = Integer.parseInt(br.readLine());
+			arr1[i] = n;
 		}
-		for (int i = 0; i < M; i++){
-			s2 = br.readLine();
-			st = new StringTokenizer(s2);
-			I = Integer.parseInt(st.nextToken());
-			J = Integer.parseInt(st.nextToken());
-			originI = arr1[I-1];
-			originJ = arr1[J-1];
-			arr1[I-1] = originJ;
-			arr1[J-1] = originI;
+		for (int i = 0; i < 30; i++){
+			arr2[i] = i+1;
 		}
-		for (int i = 0; i < N; i++){
-			System.out.print(arr1[i] + " ");
+		for (int i = 0; i < 30; i++){
+			for (int j = 0; j < 28; j++){
+                if (arr2[i] == arr1[j]) {
+                    arr3[i] = true;
+                    break;
+                }
+			}
 		}
+		for (int i = 0; i < 30; i++){
+			if (!arr3[i]){
+				System.out.println(i+1);
+			}
+		}
+
+
+
+
 
 
 
