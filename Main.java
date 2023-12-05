3c90328e051a099753347674b12034f1d78bcde5
yooInTheBuilding <dbals9926@gmail.com>
2023-12-05 15:37:24 +0900

    baek1546

--- Main.java
+++ Main.java
@@ -9,34 +9,29 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
+		int N = Integer.parseInt(br.readLine());
+		double[] arr1 = new double[N];
+		double max;
+		double sum = 0;
 		String s1 = br.readLine();
 		st = new StringTokenizer(s1);
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int I;
-		int J;
-		String s2;
-		int[] arr1 = new int[N];
-		int[] arr2 = new int[N];
 		for (int i = 0; i < N; i++){
-			arr1[i] = i+1;
-			arr2[i] = i+1;
+			arr1[i] = Integer.parseInt(st.nextToken());
 		}
-		for (int i = 0; i < M; i++){
-			s2 = br.readLine();
-			st = new StringTokenizer(s2);
-			I = Integer.parseInt(st.nextToken());
-			J = Integer.parseInt(st.nextToken());
-			for (int j = I-1; j <= J-1; j++){
-				arr1[j] = arr2[I+J-2-j];
-			}
-			for (int j = 0; j < N; j++){
-				arr2[j] = arr1[j];
+		max = arr1[0];
+		for (int i = 0; i < N; i++){
+			if (max < arr1[i]){
+				max = arr1[i];
 			}
 		}
 		for (int i = 0; i < N; i++){
-			System.out.print(arr1[i] + " ");
+			arr1[i] = (arr1[i]/max) * 100;
+			sum += arr1[i];
 		}
+		System.out.println(sum/N);
+
+
+
 
 	}
 }
