36bc3f9d25f0676ee923be3fcaa437e4136565c1
yooInTheBuilding <dbals9926@gmail.com>
2023-12-05 11:43:23 +0900

    baek10813

--- Main.java
+++ Main.java
@@ -7,30 +7,36 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 
-		String s1 = br.readLine();
+        String s1 = br.readLine();
+		StringTokenizer st;
 		st = new StringTokenizer(s1);
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int[] arr1 = new int[N];
 		int I;
 		int J;
-		int K;
+		int originI;
+		int originJ;
 		String s2;
-		for (int i = 1; i <= M; i++){
+		for (int i = 0; i < N; i++){
+			arr1[i] = i + 1;
+		}
+		for (int i = 0; i < M; i++){
 			s2 = br.readLine();
 			st = new StringTokenizer(s2);
 			I = Integer.parseInt(st.nextToken());
 			J = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			for (int j = I-1; j < J; j++){
-				arr1[j] = K;
-			}
+			originI = arr1[I-1];
+			originJ = arr1[J-1];
+			arr1[I-1] = originJ;
+			arr1[J-1] = originI;
 		}
 		for (int i = 0; i < N; i++){
 			System.out.print(arr1[i] + " ");
 		}
+
+
 
 
 
