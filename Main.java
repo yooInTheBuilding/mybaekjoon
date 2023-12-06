88115614dc2317f1780d46657c7e991bf964b5ba
yooInTheBuilding <dbals9926@gmail.com>
2023-12-06 15:02:14 +0900

    baek3003

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
 package practice_alone;
 import java.io.*;
-
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		bw.write("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
-		bw.close();
+		int[] arr1 = new int[6];
+		int[] arr2 = {1, 1, 2, 2, 2, 8};
+		String s1 = br.readLine();
+		st = new StringTokenizer(s1);
+		for (int i = 0; i < arr1.length; i++){
+			arr1[i] = arr2[i] - Integer.parseInt(st.nextToken());
+			System.out.print(arr1[i] + " ");
+		}
+
 	}
 
 
