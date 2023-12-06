aa908474289b6da04c7e6402711fdba78eee2fc7
yooInTheBuilding <dbals9926@gmail.com>
2023-12-06 14:47:52 +0900

    baek11718

--- Main.java
+++ Main.java
@@ -7,43 +7,16 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String s1 = br.readLine();
-		int sum = 0;
-		int[] arr1 = new int[s1.length()];
-		for (int i = 0; i < arr1.length; i++){
-			arr1[i] = change(s1.charAt(i));
-			sum += arr1[i] + 1;
+
+		String s1 = "";
+		while((s1 = br.readLine()) != null && !s1.isEmpty()){
+			bw.write(s1 + "\n");
 		}
-		System.out.println(sum);
+		bw.close();
 	}
-	public static char change(char var1){
-		if(var1 - 'A' == 0 || var1 - 'A' == 1
-			|| var1 - 'A' == 2){
-			return 2;
-		} else if (var1 - 'A' == 3 || var1 - 'A' == 4
-				|| var1 - 'A' == 5) {
-			return 3;
-		} else if (var1 - 'A' == 6 || var1 - 'A' == 7
-				|| var1 - 'A' == 8) {
-			return 4;
-		} else if (var1 - 'A' == 9 || var1 - 'A' == 10
-				|| var1 - 'A' == 11) {
-			return 5;
-		} else if (var1 - 'A' == 12 || var1 - 'A' == 13
-				|| var1 - 'A' == 14) {
-			return 6;
-		} else if (var1 - 'A' == 15 || var1 - 'A' == 16
-				|| var1 - 'A' == 17 || var1 - 'A' == 18) {
-			return 7;
-		} else if (var1 - 'A' == 19 || var1 - 'A' == 20
-				|| var1 - 'A' == 21) {
-			return 8;
-		} else if (var1 - 'A' == 22 || var1 - 'A' == 23
-				|| var1 - 'A' == 24 || var1 - 'A' == 25) {
-			return 9;
-		}else return var1;
-    }
+
 
 
 
