f0060e0174f9a0398b4d51e3f4ea494c7b7790fe
yooInTheBuilding <dbals9926@gmail.com>
2023-12-06 14:51:54 +0900

    baek25083

--- Main.java
+++ Main.java
@@ -6,14 +6,9 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-
-		String s1 = "";
-		while((s1 = br.readLine()) != null && !s1.isEmpty()){
-			bw.write(s1 + "\n");
-		}
+		bw.write("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
 		bw.close();
 	}
 
