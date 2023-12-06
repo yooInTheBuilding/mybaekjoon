d4e0b9f822b542a5b13587f7b31478d6c052e683
yooInTheBuilding <dbals9926@gmail.com>
2023-12-06 09:47:56 +0900

    baek9086

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
 package practice_alone;
 import java.io.*;
-
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		String s1 = br.readLine();
-		System.out.println(s1.length());
+		int T = Integer.parseInt(br.readLine());
+
+		String[] arr1 = new String[T];
+		for (int i = 0; i < T; i++){
+			arr1[i] = br.readLine();
+		}
+		for (int i = 0; i < T; i++){
+			System.out.println(String.valueOf(arr1[i].charAt(0)) + String.valueOf(arr1[i].charAt(arr1[i].length()-1)));
+		}
 
 	}
 }
