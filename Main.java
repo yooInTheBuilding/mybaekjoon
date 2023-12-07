c1b576cff5201eee0078826d525460fdc75c8be4
yooInTheBuilding <dbals9926@gmail.com>
2023-12-07 13:34:42 +0900

    baek2741

--- Main.java
+++ Main.java
@@ -8,33 +8,13 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		int C = Integer.parseInt(br.readLine());
 
-		String s1 = Integer.toString(A * B * C);
-		int[] arr1 = new int[s1.length()];
-		for (int i = 0; i < arr1.length; i++){
-			arr1[i] = Integer.parseInt(String.valueOf(s1.charAt(i)));
-
-		}
-		int[] arr2 = new int[10];
-		for (int i = 0; i < arr2.length; i++){
-			for (int j = 0; j < arr1.length; j++){
-				if (i == arr1[j]){
-					arr2[i]++;
-				}
-			}
-		}
-		for (int i = 0; i < arr2.length; i++){
-			System.out.println(arr2[i]);
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= N; i++){
+			System.out.println(i);
 		}
 
 
 
 	}
-
-
-
-
 }
