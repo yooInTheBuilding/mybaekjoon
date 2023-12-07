2461747189a5a8227449b7d148024ef38bf0f4ee
yooInTheBuilding <dbals9926@gmail.com>
2023-12-07 21:33:32 +0900

    baek15964

--- Main.java
+++ Main.java
@@ -1,43 +1,18 @@
 package practice_alone;
 import java.io.*;
-
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		double[][] grade = new double[4][3];
-		for (int i = 0; i < 4; i++){
-			for (int j = 0; j < 3; j++){
-				grade[i][j] = 3.7 + (0.3 * (2 - j)) - i;
-			}
-		}
-		String[][] gradeS = new String[4][3];
-		String sign = null;
-		for (int i = 0; i < 4; i++){
-			for (int j = 0; j < 3; j++){
-				if (j == 0){
-					sign = "+";
-				} else if (j == 1) {
-					sign = "0";
-				}else {
-					sign = "-";
-				}
-				gradeS[i][j] = (char)(65 + i) + sign;
-			}
-		}
-		String s1 = br.readLine();
-		for (int i = 0; i < 4; i++){
-			for (int j = 0; j < 3; j++){
-				if (s1.equals(gradeS[i][j])){
-					System.out.println(grade[i][j]);
-				}
-			}
-		}
-		if (s1.equals("F")){
-			System.out.println("0.0");
-		}
+		StringTokenizer st;
 
+		String s1 = br.readLine();
+		st = new StringTokenizer(s1);
+		long A = Integer.parseInt(st.nextToken());
+		long B = Integer.parseInt(st.nextToken());
+		System.out.println((A*A) - (B*B));
 	}
 }
