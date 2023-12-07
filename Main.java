002fe228011dc54142f0982ed7317c80cdcb23c0
yooInTheBuilding <dbals9926@gmail.com>
2023-12-07 16:19:08 +0900

    baek2420

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
 package practice_alone;
 import java.io.*;
-import java.time.LocalDate;
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		LocalDate now = LocalDate.now();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		System.out.println(now);
+		String s1 = br.readLine();
+		st = new StringTokenizer(s1);
+		long N = Integer.parseInt(st.nextToken());
+		long M = Integer.parseInt(st.nextToken());
+		if ((N - M) < 0){
+			System.out.println(-(N-M));
+		}else {
+			System.out.println(N - M);
+		}
 
 
 	}
