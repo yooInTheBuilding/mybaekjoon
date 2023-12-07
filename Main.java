ac364b8b31eaf4b793894752e57c843f6858578f
yooInTheBuilding <dbals9926@gmail.com>
2023-12-07 14:09:13 +0900

    baek2920

--- Main.java
+++ Main.java
@@ -1,19 +1,36 @@
 package practice_alone;
 import java.io.*;
-
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-
-		int N = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= N; i++){
-			System.out.println(i);
+		String s1 = br.readLine();
+		st = new StringTokenizer(s1);
+		int[] arr1 = new int[8];
+		for (int i = 0; i < arr1.length; i++){
+			arr1[i] = Integer.parseInt(st.nextToken());
 		}
-
+		int ascend = 0;
+		int descend = 0;
+		for (int i = 1; i < arr1.length; i++){
+			if (arr1[i] - arr1[i-1] == 1){
+				ascend++;
+			} else if (arr1[i] - arr1[i-1] == -1) {
+				descend++;
+			}
+		}
+		if (ascend == arr1.length - 1){
+			System.out.println("ascending");
+		} else if (descend == arr1.length - 1) {
+			System.out.println("descending");
+		}else {
+			System.out.println("mixed");
+		}
 
 
 	}
