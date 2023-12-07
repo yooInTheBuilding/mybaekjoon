07a43ae745f6171b13b578b63e54f4b3507bba98
yooInTheBuilding <dbals9926@gmail.com>
2023-12-07 13:22:56 +0900

    baek2577

--- Main.java
+++ Main.java
@@ -1,24 +1,34 @@
 package practice_alone;
 import java.io.*;
-import java.util.StringTokenizer;
+
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int sum = 0;
-		String s1 = br.readLine();
-		st = new StringTokenizer(s1);
-		double[] arr1 = new double[5];
+
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+
+		String s1 = Integer.toString(A * B * C);
+		int[] arr1 = new int[s1.length()];
 		for (int i = 0; i < arr1.length; i++){
-			arr1[i] = Math.pow(Integer.parseInt(st.nextToken()), 2);
-			sum += (int) arr1[i];
+			arr1[i] = Integer.parseInt(String.valueOf(s1.charAt(i)));
+
 		}
-		System.out.println(sum % 10);
-
-
+		int[] arr2 = new int[10];
+		for (int i = 0; i < arr2.length; i++){
+			for (int j = 0; j < arr1.length; j++){
+				if (i == arr1[j]){
+					arr2[i]++;
+				}
+			}
+		}
+		for (int i = 0; i < arr2.length; i++){
+			System.out.println(arr2[i]);
+		}
 
 
 
