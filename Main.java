e9ae754517c9203f86fbee51557ed0a66bdf733c
yooInTheBuilding <dbals9926@gmail.com>
2023-12-07 16:49:15 +0900

    baek2738

--- Main.java
+++ Main.java
@@ -1,23 +1,43 @@
 package practice_alone;
 import java.io.*;
-
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		int fac = 1;
-		if (N != 0) {
-			for (int i = 1; i <= N; i++) {
-				fac = fac * i;
+		String s1 = br.readLine();
+		st = new StringTokenizer(s1);
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		String s2;
+		int[][] A = new int[N][M];
+		int[][] B = new int[N][M];
+		int[][] ans = new int[N][M];
+		for (int i = 0; i < N; i++){
+			s2 = br.readLine();
+			st = new StringTokenizer(s2);
+			for (int j = 0; j < M; j++){
+				A[i][j] = Integer.parseInt(st.nextToken());
 			}
-
-		}else {
-			fac = 1;
 		}
-		System.out.println(fac);
+		for (int i = 0; i < N; i++){
+			s2 = br.readLine();
+			st = new StringTokenizer(s2);
+			for (int j = 0; j < M; j++){
+				B[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int i = 0; i < N; i++){
+			for (int j = 0; j < M; j++){
+				ans[i][j] = A[i][j] + B[i][j];
+				System.out.print(ans[i][j] + " ");
+			}
+			System.out.println("");
+		}
+
 	}
 }
