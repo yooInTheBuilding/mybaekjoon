4f8ba69eaeb28ad4a3d02c57aa93f1974e8fcc55
yooInTheBuilding <dbals9926@gmail.com>
2023-12-07 16:28:34 +0900

    baek10872

--- Main.java
+++ Main.java
@@ -1,24 +1,23 @@
 package practice_alone;
 import java.io.*;
-import java.util.StringTokenizer;
+
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 
-		String s1 = br.readLine();
-		st = new StringTokenizer(s1);
-		long N = Integer.parseInt(st.nextToken());
-		long M = Integer.parseInt(st.nextToken());
-		if ((N - M) < 0){
-			System.out.println(-(N-M));
+		int N = Integer.parseInt(br.readLine());
+		int fac = 1;
+		if (N != 0) {
+			for (int i = 1; i <= N; i++) {
+				fac = fac * i;
+			}
+
 		}else {
-			System.out.println(N - M);
+			fac = 1;
 		}
-
-
+		System.out.println(fac);
 	}
 }
